9aaee3db7d2f730a55b25c342d41ce4f432dcd64
Manish Devrani <manishdevrani777@gmail.com>
2022-03-15 19:53:49 +0530

    feat-added page routing using react-router
    fix the app links, now users can navigate through the website

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 
+import { Link } from 'react-router-dom';
+
 const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__wrapper flow-space-2">
         <p className="footer__brand">noveee</p>
         <ul className="footer__list flex">
-          <li className="footer__items">Home</li>
-          <li className="footer__items">Laptops</li>
-          <li className="footer__items">Tablets</li>
-          <li className="footer__items">Phones</li>
+        <li className="footer__items">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="footer__items">
+            <Link to="/products">Laptops</Link>
+          </li>
+          <li className="footer__items">
+            <Link to="/products">Tablets</Link>
+          </li>
+          <li className="footer__items">
+            <Link to="/products">Phones</Link>
+          </li>
         </ul>
         <p className="footer__description">
           Novee provides you with the best electronics material. We'll provide
@@ -17,9 +27,15 @@ const Footer = () => {
         </p>
         <p className="footer__copyright">Copyright 2022. All Rights Reserved</p>
         <ul className="footer__socials flex">
-          <li className="footer__items">Github</li>
-          <li className="footer__items">Twitter</li>
-          <li className="footer__items">LinkedIn</li>
+          <li className="footer__items">
+            <a href="#">Github</a>
+          </li>
+          <li className="footer__items">
+            <a href="#">Twitter</a>
+          </li>
+          <li className="footer__items">
+            <a href="#">LinkedIn</a>
+          </li>
         </ul>
       </div>
     </footer>
