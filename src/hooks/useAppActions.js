dd9ac1bf35a07fa6f3bf54e640bc754df0b7e472
Manish Devrani <manishdevrani777@gmail.com>
2022-03-20 16:27:55 +0530

    feat-added add to cart functionality

--- src/hooks/useAppActions.js
+++ src/hooks/useAppActions.js
@@ -5,8 +5,11 @@ const useAppActions = () => {
     arr.some((item) => item._id === _id);
 
   const findDiscountedPrice = (originalPrice, discountRate) => {
-    return (originalPrice - (originalPrice * Number(discountRate)) / 100).toFixed(0);
-  }  
+    return (
+      originalPrice -
+      (originalPrice * Number(discountRate)) / 100
+    ).toFixed(0);
+  };
 
   const addItemToTheWishlist = async ({
     product,
@@ -65,11 +68,43 @@ const useAppActions = () => {
     }
   };
 
+  const addProductsToCart = async ({
+    _id,
+    product,
+    currentUser,
+    stateDispatch,
+    setIsLoading,
+  }) => {
+    setIsLoading(true);
+    try {
+      const res = await axios.post(
+        "/api/user/cart",
+        { product },
+        {
+          headers: { authorization: currentUser.encodedToken },
+        }
+      );
+      console.log(res);
+      if(res.status === 200 || res.status === 201) {
+        stateDispatch({ type: 'GET_CART_DATA', payload: res.data.cart })
+      }
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const removeProductsFromCart = () => {};
+
   return {
     isAlreadyInDatabase,
     addItemToTheWishlist,
     removeItemFromWishlist,
-    findDiscountedPrice
+    findDiscountedPrice,
+    addProductsToCart,
+    removeProductsFromCart,
   };
 };
 
