7b78491710b5890e7318b6c4cbdb10dc31bf5f5c
Manish Devrani <manishdevrani777@gmail.com>
2022-05-01 22:21:26 +0530

    style-fix responsive issue in carousel

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -5,9 +5,17 @@ import { useStateContext } from '../../hooks';
 const Carousel = () => {
   const [slideWidth, setSlideWidth] = useState(undefined);
   const { state } = useStateContext();
-  const product = state?.productData[1];
+  const product = state?.productData[4];
   const carouselContainerRef = useRef();
   const slideRef = useRef();
+
+  const findNewProducts = () => {
+    return state.productData.filter((item, index) => {
+      if (index < 3) {
+        return item;
+      }
+    });
+  };
 
   const handleNavigateCarousel = (type) => {
     const maxScrollWidth = carouselContainerRef?.current?.scrollWidth;
@@ -62,32 +70,26 @@ const Carousel = () => {
                 </button>
               </div>
             </article>
-            <article className="carousel__item carousel-slide flex">
-              <div className="carousel-slide__info grid">
-                <small className="carousel-slide__label">New Product</small>
-                <h2 className="carousel-slide__name">{product?.title}</h2>
-                <p className="carousel-slide__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Deleniti, esse debitis! Quidem cumque eum illum?
-                </p>
-                <button className="carousel-slide__btn btn btn--contained-warning">
-                  <Link to={`/products/${product?._id}`}>See Product</Link>
-                </button>
-              </div>
-            </article>
-            <article className="carousel__item carousel-slide flex">
-              <div className="carousel-slide__info grid">
-                <small className="carousel-slide__label">New Product</small>
-                <h2 className="carousel-slide__name">{product?.title}</h2>
-                <p className="carousel-slide__description">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Deleniti, esse debitis! Quidem cumque eum illum?
-                </p>
-                <button className="carousel-slide__btn btn btn--contained-warning">
-                  <Link to={`/products/${product?._id}`}>See Product</Link>
-                </button>
-              </div>
-            </article>
+            {findNewProducts().map((item) => {
+              return (
+                <article
+                  key={item._id}
+                  className="carousel__item carousel-slide flex"
+                >
+                  <div className="carousel-slide__info">
+                    <small className="carousel-slide__label">New Product</small>
+                    <h2 className="carousel-slide__name">{item?.title}</h2>
+                    <p className="carousel-slide__description">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Deleniti, esse debitis! Quidem cumque eum illum?
+                    </p>
+                    <button className="carousel-slide__btn btn btn--contained-warning">
+                      <Link to={`/products/${item?._id}`}>See Product</Link>
+                    </button>
+                  </div>
+                </article>
+              );
+            })}
           </section>
           <span
             className="material-icons move-right"
