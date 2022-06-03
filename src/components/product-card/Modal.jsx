48ee8aa3d3d6e342cb840d4801819f7484853fa5
Manish Devrani <manishdevrani777@gmail.com>
2022-06-03 16:41:28 +0530

    fix-modal overflow issue in cart page

--- src/components/product-card/Modal.jsx
+++ src/components/product-card/Modal.jsx
@@ -14,49 +14,65 @@ const Modal = ({
 }) => {
   const { moveToWishlist } = useAppActions();
   return (
-    <section className="dialog dialog--alert" style={{ inlineSize: '350px' }}>
-      <div className="dialog__content">
-        <button
-          type="button"
-          className="material-icons dialog__clear"
-          onClick={() => setCurrentId('')}>
-          clear
-        </button>
-        <div className="dialog__info wrapper">
-          <h3 className="dialog__header">Dialog Header</h3>
-        </div>
-        <div className="dialog__cta wrapper">
+    <section className="dialog dialog--alert">
+      <button
+        type="button"
+        className="dialog-overlay"
+        style={{ zIndex: '20' }}
+        onClick={() => setCurrentId('')}>
+        &nbsp;
+      </button>
+      <div
+        className="dialog-data"
+        style={{ zIndex: '21', left: '50%', inlineSize: '350px' }}>
+        <div
+          className="dialog__content"
+          style={{
+            padding: '0.5rem',
+            paddingBlockStart: '2rem',
+          }}>
           <button
             type="button"
-            className="dialog__action-btn"
-            onClick={() => {
-              removeProductsFromCart({
-                _id,
-                stateDispatch,
-                setIsLoading,
-                setDisableButton,
-              });
-              setCurrentId('');
-            }}
-            disabled={disableButton}>
-            Remove
+            className="material-icons dialog__clear"
+            onClick={() => setCurrentId('')}>
+            clear
           </button>
-          <button
-            type="button"
-            className="dialog__action-btn"
-            onClick={() => {
-              moveToWishlist({
-                _id,
-                stateDispatch,
-                setIsLoading,
-                product,
-                setDisableButton,
-              });
-              setCurrentId('');
-            }}
-            disabled={disableButton}>
-            Move To Wishlist
-          </button>
+          <div className="dialog__info wrapper">
+            <h3 className="dialog__header">What you like to do?</h3>
+          </div>
+          <div className="dialog__cta wrapper">
+            <button
+              type="button"
+              className="dialog__action-btn"
+              onClick={() => {
+                removeProductsFromCart({
+                  _id,
+                  stateDispatch,
+                  setIsLoading,
+                  setDisableButton,
+                });
+                setCurrentId('');
+              }}
+              disabled={disableButton}>
+              Remove
+            </button>
+            <button
+              type="button"
+              className="dialog__action-btn"
+              onClick={() => {
+                moveToWishlist({
+                  _id,
+                  stateDispatch,
+                  setIsLoading,
+                  product,
+                  setDisableButton,
+                });
+                setCurrentId('');
+              }}
+              disabled={disableButton}>
+              Move To Wishlist
+            </button>
+          </div>
         </div>
       </div>
     </section>
