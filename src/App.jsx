7700ad6763562315fab93127715734ddef3815ea
Manish Devrani <manishdevrani777@gmail.com>
2022-06-03 17:28:01 +0530

    added profile routes in pritected routes

--- src/App.jsx
+++ src/App.jsx
@@ -77,12 +77,54 @@ const App = () => {
               <UserProfile />
             </ProtectedRoute>
           }>
-          <Route index element={<Overview />} />
-          <Route path="address" element={<AddressPage />} />
-          <Route path="orders" element={<OrdersPage />} />
-          <Route path="coupons" element={<CouponsPage />} />
-          <Route path="edit" element={<ProfilePage />} />
-          <Route path="settings" element={<Settings />} />
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Overview />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="address"
+            element={
+              <ProtectedRoute>
+                <AddressPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="orders"
+            element={
+              <ProtectedRoute>
+                <OrdersPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="coupons"
+            element={
+              <ProtectedRoute>
+                <CouponsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="edit"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="settings"
+            element={
+              <ProtectedRoute>
+                <Settings />
+              </ProtectedRoute>
+            }
+          />
         </Route>
 
         <Route path="*" element={<NotFound />} />
