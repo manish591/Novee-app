bec14573eca40e324a8751fba1bee8a43d6d3182
Manish Devrani <manishdevrani777@gmail.com>
2022-08-15 14:20:55 +0530

    refactor-splitted main reducer into smaller reducer fuction

---
+++ src/reducer/index.js
@@ -0,0 +1,6 @@
+export {
+  filterReducer,
+  initialFilterState,
+} from './filter-reducer/filter-reducer';
+export { dataReducer, initialDataState } from './data-reducer/data-reducer';
+export { stateReducer } from './main-reducer/state-reducer';
